81ce0b4c3212819cf04ae7d11abf017a0a0a64e3
njscd <your-email@example.com>
2025-03-19 12:12:27 +0500

    variables

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// const classData = {
-//     className : "2nd year",
-//     totalStudents : 34,
-//     totsalMarks : 300,
-//     isPass : true,
-// };
-// // classData["totalStudents"] = classData["totalStudents"] + 4
-// classData["className"] = "3rd year"
-// // console.log(classData["className"]);
+const classData = {
+    className : "2nd year",
+    totalStudents : 34,
+    totsalMarks : 300,
+    isPass : true,
+};
+// classData["totalStudents"] = classData["totalStudents"] + 4
+classData["className"] = "3rd year"
+// console.log(classData["className"]);
 
 
 
-// const imranAli = {
-//     fullNmae : "imran Ali",
-//     className : "final Samister",
-//     obtainMArks : 99,
-//     result : true,
-// };
+const imranAli = {
+    fullNmae : "imran Ali",
+    className : "final Samister",
+    obtainMArks : 99,
+    result : true,
+};
 
-// imranAli["result"] = false
+imranAli["result"] = false
 
-// // imranAli.fullName = "imran"
-// // console.log(typeof imranAli.result);
+imranAli.fullName = "imran"
+console.log(typeof imranAli.result);
 
-// // console.log(imranAli);
+console.log(imranAli);
 
-// let noValue = null;
-// console.log(noValue);
+let noValue = null;
+console.log(noValue);
 
 
-// let sym = Symbol('description');
+let sym = Symbol('description');
 
-// console.log(typeof sym);
+console.log(typeof sym);
